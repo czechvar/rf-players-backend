0319774830debf2ecddbd47f4b8e85f26d3c6e62
czechvar <janantl@gmail.com>
2025-09-30 16:48:27 +0200

    Fixins vercel prod hell

--- rf-payload/src/payload.config.ts
+++ rf-payload/src/payload.config.ts
@@ -33,11 +33,11 @@ export default buildConfig({
    * CORS / CSRF - Allow frontend access
    */
   cors: [
-    ...(isDevelopment ? ["http://localhost:4000", "http://localhost:3000"] : []),
+    ...(isDevelopment ? ["*"] : []),
     ...(process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [])
   ],
   csrf: [
-    ...(isDevelopment ? ["http://localhost:4000", "http://localhost:3000"] : []),
+    ...(isDevelopment ? ["*"] : []),
     ...(process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [])
   ],
   typescript: {
